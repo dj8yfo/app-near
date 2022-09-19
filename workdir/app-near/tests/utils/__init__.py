2851cf14acab2d1c6cbc0f0486a1bf33aadaa7f6
Jean-Baptiste Bédrune <jean-baptiste.bedrune@ledger.fr>
2022-09-19 14:55:43 +0200

    tests: minor changes to improve readability

--- workdir/app-near/tests/utils/__init__.py
+++ workdir/app-near/tests/utils/__init__.py
@@ -1,10 +1,10 @@
 from mnemonic import Mnemonic
 
-mnemo = Mnemonic("english")
+DEFAULT_SPECULOS_MNEMONIC = "glory promote mansion idle axis finger extra february uncover one " \
+                            "trip resource lawn turtle enact monster seven myth punch hobby " \
+                            "comfort wild raise skin"
 
-DEFAULT_SPECULOS_MNEMONIC = "glory promote mansion idle axis finger extra february uncover one trip resource lawn turtle enact monster seven myth punch hobby comfort wild raise skin"
-
-default_settings = {
+DEFAULT_SETTINGS = {
     # mnemonic to use when running speculos
     "mnemonic": DEFAULT_SPECULOS_MNEMONIC,
     # path of the automation file to use for speculos if used, or None
@@ -17,17 +17,13 @@ default_settings = {
 def automation(filename):
     """Decorator that adds the automation_file attribute to a test function.
 
-    When present, this filename will be used as the --automation file when creating the speculos fixture.
+    When present, this filename will be used as the --automation file when creating the
+    Speculos fixture.
     """
     def decorator(func):
         func.automation_file = filename
         return func
     return decorator
-
-
-def mnemonic(mnemo: str):
-    """Adds the `mnemonic` setting to the test settings."""
-    return test_settings({"mnemonic": mnemo})
 
 
 class SpeculosGlobals:
@@ -36,5 +32,4 @@ class SpeculosGlobals:
             raise ValueError(f"Invalid network: {network}")
 
         self.mnemonic = mnemonic
-        self.seed = mnemo.to_seed(mnemonic)
-
+        self.seed = Mnemonic("english").to_seed(mnemonic)
